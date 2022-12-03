cc7808db595f81a418ede4a56fb0c8d4b3eda4e2
Gidaio <killshade@gmail.com>
2022-12-03 09:16:40 -0800

    Did more error handling stuff.

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,2 +1,44 @@
 pub mod part1;
 pub mod part2;
+
+use std::error;
+use std::fmt;
+
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+
+#[derive(Debug)]
+struct PriorityError(u8);
+
+impl fmt::Display for PriorityError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Unrecognized character {}", self.0)
+    }
+}
+
+impl error::Error for PriorityError {}
+
+#[derive(Debug)]
+struct BadCommonItems(usize);
+
+impl fmt::Display for BadCommonItems {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Bad number of common items. Expected 1, got {}", self.0)
+    }
+}
+
+impl error::Error for BadCommonItems {}
+
+const LITTLE_A: u8 = 'a' as u8;
+const LITTLE_Z: u8 = 'z' as u8;
+const BIG_A: u8 = 'A' as u8;
+const BIG_Z: u8 = 'Z' as u8;
+
+fn get_priority_of_item(item: u8) -> Result<usize> {
+    if item >= LITTLE_A && item <= LITTLE_Z {
+        Ok((item - LITTLE_A + 1) as usize)
+    } else if item >= BIG_A && item <= BIG_Z {
+        Ok((item - BIG_A + 27) as usize)
+    } else {
+        Err(PriorityError(item).into())
+    }
+}
--- src/day3/part2.rs
+++ src/day3/part2.rs
@@ -1,25 +1,20 @@
 use std::collections::HashSet;
-use std::error::Error;
 use std::fmt;
 use std::fs;
 use std::io::{self, BufRead};
 
-type Result<T> = std::result::Result<T, Box<dyn Error>>;
-#[derive(Debug)]
-struct PriorityError(u8);
+use super::*;
 
-impl fmt::Display for PriorityError {
+#[derive(Debug)]
+struct WrongNumberOfRucksacks;
+
+impl fmt::Display for WrongNumberOfRucksacks {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Unrecognized character {}", self.0)
+        write!(f, "Number of rucksacks was not a multiple of three!")
     }
 }
 
-impl Error for PriorityError {}
-
-const LITTLE_A: u8 = 'a' as u8;
-const LITTLE_Z: u8 = 'z' as u8;
-const BIG_A: u8 = 'A' as u8;
-const BIG_Z: u8 = 'Z' as u8;
+impl error::Error for WrongNumberOfRucksacks {}
 
 pub fn get_priority_of_team_badges() -> Result<usize> {
     let file = fs::File::open("inputs/day3.txt")?;
@@ -29,29 +24,29 @@ pub fn get_priority_of_team_badges() -> Result<usize> {
     let mut priority_sum: usize = 0;
 
     loop {
-        let first_rucksack: HashSet<u8> = if let Some(i) = lines.next() {
-            HashSet::from_iter(i?.bytes())
+        let first_rucksack = if let Some(i) = lines.next() {
+            HashSet::<u8>::from_iter(i?.bytes())
         } else {
             break;
         };
 
-        let second_rucksack: HashSet<u8> = if let Some(i) = lines.next() {
-            HashSet::from_iter(i?.bytes())
+        let second_rucksack = if let Some(i) = lines.next() {
+            Ok(HashSet::<u8>::from_iter(i?.bytes()))
         } else {
-            panic!("Number of rucksacks was not a multiple of three!");
-        };
+            Err(WrongNumberOfRucksacks)
+        }?;
 
-        let third_rucksack: HashSet<u8> = if let Some(i) = lines.next() {
-            HashSet::from_iter(i?.bytes())
+        let third_rucksack = if let Some(i) = lines.next() {
+            Ok(HashSet::<u8>::from_iter(i?.bytes()))
         } else {
-            panic!("Number of rucksacks was not a multiple of three!");
-        };
+            Err(WrongNumberOfRucksacks)
+        }?;
 
         let mut common_items = first_rucksack;
         common_items.retain(|item| second_rucksack.contains(item) && third_rucksack.contains(item));
 
         if common_items.len() != 1 {
-            panic!("Not one common item found! {:?}", common_items);
+            return Err(BadCommonItems(common_items.len()).into());
         }
 
         for item in common_items {
@@ -60,14 +55,4 @@ pub fn get_priority_of_team_badges() -> Result<usize> {
     }
 
     Ok(priority_sum)
-}
-
-fn get_priority_of_item(item: u8) -> Result<usize> {
-    if item >= LITTLE_A && item <= LITTLE_Z {
-        Ok((item - LITTLE_A + 1) as usize)
-    } else if item >= BIG_A && item <= BIG_Z {
-        Ok((item - BIG_A + 27) as usize)
-    } else {
-        Err(PriorityError(item).into())
-    }
 }
